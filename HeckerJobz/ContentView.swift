a498940b4c3bc77317a2e7bc2cf7c8220f4bc09f
Avi Kasliwal <avi.kasliwal04@gmail.com>
2022-09-11 21:15:20 +0530

    Initial Commit

---
+++ HeckerJobz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HeckerJobz
+//
+//  Created by Avi Kasliwal on 11/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HeckerJobz/HeckerJobzApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeckerJobzApp.swift
+//  HeckerJobz
+//
+//  Created by Avi Kasliwal on 11/09/22.
+//
+
+import SwiftUI
+
+@main
+struct HeckerJobzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
